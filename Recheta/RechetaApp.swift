e1df225b05b6b4f6b3c8cf11c8f4b4beb8b5c03f
Alonso Huerta <alonso12_284@outlook.com>
2024-01-01 13:40:58 -0600

    Initial Commit

---
+++ Recheta/RechetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RechetaApp.swift
+//  Recheta
+//
+//  Created by Alonso Huerta on 01/01/24.
+//
+
+import SwiftUI
+
+@main
+struct RechetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
